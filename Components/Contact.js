9339cf263344fd82894c66f8543e9cf0b30a2927
Piotr Wlazeł <pancu1222@gmail.com>
2019-08-20 17:00:19 +0200

    changed to JSX

---
+++ Components/Contact.js
@@ -0,0 +1,20 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return (
+      <div className={"contactItem"}>
+        <img
+          className={"contactImage"}
+          src={
+            "https://cdn2.iconfinder.com/data/icons/picons-basic-1/57/basic1-039_address_book-512.png"
+          }
+        />
+        <p className={"contactLabel"}>Imię: {this.props.item.firstName}</p>
+        <p className={"contactLabel"}>Nazwisko: {this.props.item.lastName}</p>
+        <a href={"mailto:" + this.props.item.email}>{this.props.item.email}</a>
+      </div>
+    );
+  }
+});
---
+++ Components/ContactForm.js
@@ -0,0 +1,27 @@
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return (
+      <form className={"contactForm"}>
+        <input
+          type={"text"}
+          placeholder={"First Name"}
+          value={this.props.contact.firstName}
+        />
+        <input
+          type={"text"}
+          placeholder={"Last Name"}
+          value={this.props.contact.lastName}
+        />
+        <input
+          type={"text"}
+          placeholder={"Email"}
+          value={this.props.contact.email}
+        />
+        <button type={"submit"}>Add contact</button>
+      </form>
+    );
+  }
+});
